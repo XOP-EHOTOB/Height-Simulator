2632257daab484182181e7f3d92484713c216918
XOP-EHOTOB <dima.dimitrev@gmail.com>
2020-10-03 23:39:27 +0300

    Updates

---
+++ precache-manifest.4aec85a018e67b0896f24cb628a226cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25f7c90b3ed64d796c502526813376a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d0f11901d1298e246bf",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9d0f11901d1298e246bf",
+    "url": "./static/js/2.25e1961a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.25e1961a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3e9d838533d5ee1274",
+    "url": "./static/js/main.20ee7ae8.chunk.js"
+  },
+  {
+    "revision": "6e5f4c5f7db4efb565d4",
+    "url": "./static/js/runtime-main.f89affb8.js"
+  }
+]);
